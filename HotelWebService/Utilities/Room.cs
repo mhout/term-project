4b323c2c32bf7ff6850688373d21ede7f5af9eff
evanshoshan <evan.shoshan@gmail.com>
2018-04-03 20:51:43 -0400

    added classes

---
+++ HotelWebService/Utilities/Room.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    class Room
+    {
+        private int roomNumber;
+        private string roomDescription;
+        private string roomType;
+        private int numberBeds;
+        private int sleeps;
+        private string view;
+        private double rate;
+        private string status;
+
+        public int RoomNumber { get => roomNumber; set => roomNumber = value; }
+        public string RoomDescription { get => roomDescription; set => roomDescription = value; }
+        public string RoomType { get => roomType; set => roomType = value; }
+        public int NumberBeds { get => numberBeds; set => numberBeds = value; }
+        public int Sleeps { get => sleeps; set => sleeps = value; }
+        public string View { get => view; set => view = value; }
+        public double Rate { get => rate; set => rate = value; }
+        public string Status { get => status; set => status = value; }
+
+        public Room()
+        {
+
+        }
+    }
+}
---
+++ HotelWebService/Utilities/customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    class Customer
+    {
+        private string name;
+        private string phone;
+        private string email;
+
+        public string Name { get => name; set => name = value; }
+        public string Phone { get => phone; set => phone = value; }
+        public string Email { get => email; set => email = value; }
+
+        public Customer()
+        {
+
+        }
+    }
+}
